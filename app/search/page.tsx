1afb28eb11595d7958bbc529ab2ee8e222e7da04
Ramazan Arıcı <ramazanarc5@gmail.com>
2024-03-20 16:12:05 +0300

    Oxylabs Web Scraper API

---
+++ app/search/page.tsx
@@ -0,0 +1,28 @@
+import { fetchResults } from "@/lib/fetchResults";
+import { notFound } from "next/navigation";
+
+type Props = {
+  searchParams: SearchParams;
+};
+
+export type SearchParams = {
+  url: URL;
+  group_adults: string;
+  group_children: string;
+  no_rooms: string;
+  checkin: string;
+  checkout: string;
+};
+
+async function SearchPage({ searchParams }: Props) {
+  if (!searchParams.url) return notFound();
+
+  const results = await fetchResults(searchParams);
+
+  if (!results) return <div>No Results...</div>;
+
+  console.log(results);
+  return <div>SearchPage</div>;
+}
+
+export default SearchPage;
